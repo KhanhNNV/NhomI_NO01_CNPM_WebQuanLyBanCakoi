4f4a1b73459e439dd602a5296a19b659424c242f
KhanhNNV <khanhnnv230905@gmail.com>
2024-10-23 16:41:38 +0700

    fix program

--- WebKoiFarmShop/KoiFarmShop.WebApplication/Program.cs
+++ WebKoiFarmShop/KoiFarmShop.WebApplication/Program.cs
@@ -1,8 +1,10 @@
 using KoiFarmShop.Repositories.Entities;
-using KoiFarmShop.Repositories.Interface;
 using KoiFarmShop.Repositories;
-using KoiFarmShop.Services.Interface;
 using KoiFarmShop.Services;
+using KoiFarmShop.Repositories.InterfaceRepository;
+using KoiFarmShop.Repositories.Repositories;
+using KoiFarmShop.Services.InterfaceService;
+using KoiFarmShop.Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,9 @@ builder.Services.AddRazorPages();
 //DI
 builder.Services.AddDbContext<KoiFarmShopDbContext>();
 //DI repositories
-builder.Services.AddScoped<IRoleRepositories, RoleRepositories>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 //DI services
-builder.Services.AddScoped<IRoleServices, RoleServices>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
