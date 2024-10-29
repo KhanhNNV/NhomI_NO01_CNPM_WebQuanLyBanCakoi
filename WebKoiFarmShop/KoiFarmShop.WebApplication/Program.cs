44870afdba6a24703d9315ab4460f234fb25332d
KhanhNNV <khanhnnv230905@gmail.com>
2024-10-29 12:57:25 +0700

    orderdetail

--- WebKoiFarmShop/KoiFarmShop.WebApplication/Program.cs
+++ WebKoiFarmShop/KoiFarmShop.WebApplication/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddScoped<ICmtBlogRepository, CmtBlogRepository>();
 builder.Services.AddScoped<INewRepository, NewsRepository>();
 builder.Services.AddScoped<ICmtNewsRepository, CmtNewsRepository>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
 //DI services
 builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -42,6 +43,7 @@ builder.Services.AddScoped<ICmtBlogService, CmtBlogService>();
 builder.Services.AddScoped<INewsService, NewsService>();
 builder.Services.AddScoped<ICmtNewsService, CmtNewsService>();
 builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddScoped<IOrderDetailService, OrderDetailService>();
 
 
 
